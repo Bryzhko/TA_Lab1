c56005378afd0b382ee07955c4ab9ffc6d51bea2
Bryzhko <beasumy@gmail.com>
2023-05-16 23:14:30 +0300

    Clone Commit

---
+++ src/main/java/com/fluffy/universe/utils/DataSource.java
@@ -0,0 +1,25 @@
+package com.fluffy.universe.utils;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.sql.SQLException;
+
+public final class DataSource {
+    private static final HikariConfig dataSourceConfiguration = new HikariConfig();
+    private static final Sql2o dataSource;
+
+    private DataSource() {}
+
+    static {
+        dataSourceConfiguration.setJdbcUrl(String.format("jdbc:sqlite:/%s", Configuration.get("database.filename")));
+        //dataSourceConfiguration.setJdbcUrl("jdbc:sqlite:C:\\Users\\EduardBryzhko\\IdeaProjects\\universe-master\\universe.db");
+        dataSource = new Sql2o(new HikariDataSource(dataSourceConfiguration));
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.open();
+    }
+}
